d041719d75c1db452190fc9f9b03795efb69a40e
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-10-31 15:08:31 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,26 @@
+/*
+ * Programmer: Henrik A. Christensen     Date Completed: in progress
+ * Instructor: Kurt Nørmark              Class:          Imperative Programming
+ *
+ * Write a program to take a product code from Millie's Mail-Order Catalog
+ * seperate it into its component part.
+ *
+ * An MMOC product code begins with one or more letters identifying the warehouse
+ * where the product is stored.
+ * Next come the one or more digits that are the product ID.
+ * The final field of the string starts with a capital letter and represents
+ * qualifiers such as size, color and so on.
+ *
+ * Example: ATL1203S14
+ *
+ * Product: 1203
+ * Size: 14
+ * Warehouse: ATL (Atlanta)
+*/
+
+#include <stdlib.h>
+
+int main(void)
+{
+  return EXIT_SUCCESS;
+}
