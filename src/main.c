732d6d1c9b60e80cae70a60be558d3edd292c86f
Henrik Abel Christensen <sensen1695@hotmail.com>
2018-11-01 18:12:31 +0100

    created mvp

--- src/main.c
+++ src/main.c
@@ -18,9 +18,39 @@
  * Warehouse: ATL (Atlanta)
 */
 
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
+  int i;
+  char warehouse[25], prod_id[25], qualifier[25];
+  char str_in[] = "ATL1203S14";
+
+  for (i = 0; i < strlen(warehouse); i++)
+  {
+    warehouse[i] = '\0';
+    prod_id[i] = '\0';
+    qualifier[i] = '\0';
+  }
+
+  while (strlen(qualifier) < 1)
+  {
+    if (isdigit(str_in[i]) && strlen(warehouse) < 1)
+      strncat(warehouse, &str_in[0], i);
+    else if (strlen(warehouse) > 0 && !isdigit(str_in[i]) && isupper(str_in[i]))
+      strncat(prod_id, &str_in[strlen(warehouse)], i - strlen(warehouse));
+    else if (strlen(prod_id) > 0)
+      strncat(qualifier, &str_in[strlen(warehouse) + strlen(prod_id)], strlen(str_in) - strlen(prod_id) - strlen(warehouse));
+
+    i++;
+  }
+
+  printf("\nWarehouse: %s", warehouse);
+  printf("\nID: %s", prod_id);
+  printf("\nQualifier: %s", qualifier);
+
   return EXIT_SUCCESS;
 }
